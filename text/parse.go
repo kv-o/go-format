a9d69532b5b55de7e14efefa2de04e2e1b8d4b3f
kvo <kvo@zotu.dev>
2024-06-10 16:06:43 +0930

    all: rename package to "go-format"
    Signed-off-by: kvo <kvo@envs.net>

--- text/parse.go
+++ text/parse.go
@@ -8,7 +8,7 @@ package text
 import (
 	"io"
 
-	"git.sr.ht/~kvo/format/document"
+	"git.sr.ht/~kvo/go-format/document"
 )
 
 // Parse returns a pointer to a document.Node representing the contents of the
--- text/render.go
+++ text/render.go
@@ -7,9 +7,9 @@ import (
 	"strconv"
 	"strings"
 
-	"git.sr.ht/~kvo/format/document"
-	"git.sr.ht/~kvo/libgo/defs"
-	"git.sr.ht/~kvo/libgo/errors"
+	"git.sr.ht/~kvo/go-format/document"
+	"git.sr.ht/~kvo/go-std/defs"
+	"git.sr.ht/~kvo/go-std/errors"
 )
 
 type listConfig struct {
